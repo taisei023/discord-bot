b7df60d3fe0445e7c159fd9a09687c69231e3d37
taisei023 <tm.lovin8@gmail.com>
2025-02-18 21:10:50 +0900

    #17 読み上げ機能の追加

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import discord
 from config import TOKEN
 from commands import setup_commands  
 from message_listener import handle_message
-from time_signal import TimeSignal  # 時報機能をインポート
+from time_signal import TimeSignal  # 時報機能
+from voice_chat_reader import VoiceChatReader  # VC文字読み上げ機能
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,7 +13,8 @@ class MyBot(discord.Client):
     def __init__(self):
         super().__init__(intents=intents)
         self.tree = discord.app_commands.CommandTree(self)
-        self.time_signal = TimeSignal(self)  # 時報機能を初期化
+        self.time_signal = TimeSignal(self)  # 時報機能
+        self.voice_chat_reader = VoiceChatReader(self,speed=2.0)  # VC速度はここで変える
 
     async def setup_hook(self):
         await setup_commands(self)
@@ -30,10 +32,11 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     await handle_message(bot, message)
+    await bot.voice_chat_reader.on_message(message)  # VC文字読み上げ機能を呼び出す
 
 @bot.event
 async def on_voice_state_update(member, before, after):
-    """ ボットのVC接続状態が変化したときに `time_signal.py` の処理を実行 """
+    """ ボットのVC接続状態が変化したときに time_signal.py の処理を実行 """
     await bot.time_signal.on_voice_state_update(member, before, after)
 
 bot.run(TOKEN)
